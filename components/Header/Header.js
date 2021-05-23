c432c94e11df0e432d52cc5bae3171113b4214d1
Maná Machado <manamachado8@gmail.com>
2021-05-23 00:58:44 -0300

    feat: topbar fixed component

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -5,11 +5,13 @@ import BannerMobile from './BannerMobile';
 import BannerRuler from './BannerRuler';
 import Topbar from './Topbar';
 import Menu from './Menu';
+import TopbarFixed from './TopbarFixed';
 
 function Header({ categories }) {
   return (
     <header className="header">
       <Topbar />
+      <TopbarFixed />
       <Menu categories={categories} />
       {isMobile
         ? <BannerMobile />
