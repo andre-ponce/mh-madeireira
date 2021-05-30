9aeab464327a16ddb0bfc3fcc97302a6605c293f
Maná Machado <manamachado8@gmail.com>
2021-05-30 11:47:54 -0300

    fix: topbar fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import Head from 'next/head';
 import AOS from 'aos';
+import $ from 'jquery';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'aos/dist/aos.css';
@@ -10,6 +11,67 @@ import '../styles/all.scss';
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
+    function TornarHeaderFixo() {
+    // Caso seja menor que 1000px, não faça nada
+      if (window.innerWidth <= 1000) {
+        return;
+      }
+
+      $(window).scroll(() => {
+        if ($(window).scrollTop() === 0) {
+          $('.header__topbar--fixed').addClass('d-none');
+          $('.header__menu').removeClass('fixed');
+        } else {
+          $('.header__menu').addClass('fixed');
+          $('.header__topbar--fixed').removeClass('d-none');
+        }
+      });
+    }
+
+    function OrganizarMenu() {
+      if ($(window).innerWidth < 992) {
+        return;
+      }
+
+      const larguraContainer = $('.categorias_container--sd').width();
+      const categorias = $('.categorias_container--sd > li');
+      const margem = 20;
+
+      let larguraCategorias = 0;
+
+      categorias.each(function () {
+        if ($(this).width() > 0 && $(this).is(':visible')) {
+          larguraCategorias += ($(this).innerWidth() + margem);
+        }
+      });
+
+      function VerificarSeEMaior() {
+        if (larguraContainer <= larguraCategorias) {
+          AddAoVerTudo();
+        }
+      }
+
+      VerificarSeEMaior();
+
+      function AddAoVerTudo() {
+        larguraCategorias -= $(categorias).last().width();
+
+        $('.categorias_container--sd .see-all__submenus')
+          .append($('.categorias_container--sd > li').last());
+
+        VerificarSeEMaior();
+      }
+    }
+
+    $(document).ready(() => {
+      OrganizarMenu();
+      TornarHeaderFixo();
+    });
+
+    $(window).resize(() => {
+      OrganizarMenu();
+    });
+
     AOS.init();
   }, []);
 
