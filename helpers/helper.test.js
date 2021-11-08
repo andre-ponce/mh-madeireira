e7a3acb99ae116cc7ed3b7ac75e15c33959526ec
Pedro Ramon <pedroramon.dev@gmail.com>
2021-11-08 16:15:45 -0300

    feat: add format helpers

---
+++ helpers/helper.test.js
@@ -0,0 +1,29 @@
+import { format } from "."
+
+test('format.telephone() shold return empty', () => {
+  expect(format.telephone()).toBe('');
+});
+
+test('format.telephone("055 99 9 9999 9999") shold return (99) 9 9999-9999', () => {
+  expect(format.telephone('055 99 9 9999 9999')).toBe('(99) 9 9999-9999');
+});
+
+test('format.telephone("055 99 9999 9999") shold return (99) 9999-9999', () => {
+  expect(format.telephone('055 99 9999 9999')).toBe('(99) 9999-9999');
+});
+
+test('format.telephone("99 9999 9999") shold return (99) 9999-9999', () => {
+  expect(format.telephone('99 9999 9999')).toBe('(99) 9999-9999');
+});
+
+test('format.telephone("99999999999") shold return (99) 9 9999-9999', () => {
+  expect(format.telephone('99999999999')).toBe('(99) 9 9999-9999');
+});
+
+test('format.telephone("9999999999") shold return (99) 9999-9999', () => {
+  expect(format.telephone('9999999999')).toBe('(99) 9999-9999');
+});
+
+test('format.telephone("055 11 3 1234 1234") shold return empty', () => {
+  expect(format.telephone('055 11 3 1234 1234')).toBe('');
+});
---
+++ helpers/index.js
@@ -0,0 +1,29 @@
+export const format = {
+  telephone(number) {
+
+    if (!number) return '';
+
+    const onlyNumbers = number.replace(/[^0-9]+/g, '');
+
+    const [
+      full,
+      country,
+      ddd,
+      nine,
+      phoneStar,
+      phoneEnd
+    ] = /(0?55)?(\d\d)(\d)?(\d{4})(\d{4})$/.exec(onlyNumbers);
+
+    if (nine != undefined && nine != '9') return '';
+
+    return `(${ddd}) ${nine || ''} ${phoneStar}-${phoneEnd}`.replace(/\s{2,}/, ' ');
+  },
+
+  currency(value) {
+    return Intl.NumberFormat("pt-BR", {
+      style: 'currency',
+      currency: "BRL",
+      maximumFractionDigits: 2
+    }).format(value);
+  }
+}
