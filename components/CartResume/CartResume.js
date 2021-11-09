6daec30bf66fc4ad17058e16a3ebe49179e413b9
Pedro Ramon <pedroramon.dev@gmail.com>
2021-11-09 14:42:05 -0300

    feat: treat cart resume to deal with empty cart

--- components/CartResume/CartResume.js
+++ components/CartResume/CartResume.js
@@ -1,4 +1,5 @@
 import CartResumeItem from "./CartResumeItem";
+import { isEmpty } from "lodash";
 
 const itens = [
   {
@@ -28,32 +29,44 @@ export default function CartResume() {
   return (
     <div className="cart">
       <img src="/images/cart.svg" alt="Carrinho" />
-      <span className="cart__qtd">99</span>
-
-      <div className="cart__cart-container block_hover">
-        <span className="cart-container__title">RESUMO DO SEU CARRINHO</span>
-        <ul className="cart-container__items">
-          {
-            itens.map(item =>
-              <CartResumeItem item={item} />)
-          }
-        </ul>
-        <div className="cart-container__subtotal">
-          <span className="subtotal__title">SUBTOTAL</span>
-          <div className="subtotal__prices">
-            <span className="prices__price">R$ 999,99</span>
-            <span className="prices__installments">
-              Em até
-              <strong>12x</strong>
-              de
-              <strong>R$ R$ 999,99</strong>
-            </span>
-          </div>
-        </div>
-        <div className="cart-container__actions">
-          <a href="/" className="actions__return">Ir para o carrinho</a>
-          <a href="/" className="actions__buy"> FINALIZAR COMPRA </a>
-        </div>
+      <span className="cart__qtd">{itens.length}</span>
+      <div className={`cart__cart-container block_hover ${isEmpty(itens) ? 'cart__cart-container--empty' : ''}`}>
+        {
+          isEmpty(itens)
+            ? (
+              <>
+                <span className="cart-container__title">SEU CARRINHO ESTÁ VAZIO</span>
+                <img src="/images/cart-empty.png" alt="carrinho vazio" />
+              </>
+            )
+            : (
+              <>
+                <span className="cart-container__title">RESUMO DO SEU CARRINHO</span>
+                <ul className="cart-container__items">
+                  {
+                    itens.map(item =>
+                      <CartResumeItem item={item} />)
+                  }
+                </ul>
+                <div className="cart-container__subtotal">
+                  <span className="subtotal__title">SUBTOTAL</span>
+                  <div className="subtotal__prices">
+                    <span className="prices__price">R$ 999,99</span>
+                    <span className="prices__installments">
+                      Em até
+                      <strong>12x</strong>
+                      de
+                      <strong>R$ R$ 999,99</strong>
+                    </span>
+                  </div>
+                </div>
+                <div className="cart-container__actions">
+                  <a href="/" className="actions__return">Ir para o carrinho</a>
+                  <a href="/" className="actions__buy"> FINALIZAR COMPRA </a>
+                </div>
+              </>
+            )
+        }
       </div>
     </div>
   );
