b4c1cc8d0120393afe09c8cd48e82089770ea8b2
Pedro Ramon <pedroramon.dev@gmail.com>
2021-11-11 09:12:13 -0300

    style: remove css module from institutional page

--- components/InstitutionalMain/InstitutionalMain.js
+++ components/InstitutionalMain/InstitutionalMain.js
@@ -1,11 +1,9 @@
-import style from './InstitutionalMain.module.scss';
-
 function InstitutionalMain({ titulo, conteudo }) {
   return (
-    <main className={style.main}>
+    <main className="main intitutional">
       <section className="container_serie-ds">
         <h2 className="page-title">{titulo}</h2>
-        <div className={style.content} dangerouslySetInnerHTML={{__html: conteudo}} />
+        <div className="intitutional__content" dangerouslySetInnerHTML={{__html: conteudo}} />
       </section>
     </main>
   );
