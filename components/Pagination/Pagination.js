aa7053f92b1a335757b253d936b45abda9022eb1
Pedro Ramon <pedroramon.dev@gmail.com>
2021-11-18 10:19:39 -0300

    refactor: isolating pagination link component

--- components/Pagination/Pagination.js
+++ components/Pagination/Pagination.js
@@ -1,40 +1,11 @@
 import React from 'react';
-import Link from 'next/link';
+import { PaginationItem } from './PaginationItem';
 
-function Pagination({currentPage, isLastPage, nextHref, prevHref}) {
+function Pagination({ currentPage, isLastPage, nextHref, prevHref }) {
   return (
-    // <ul >
-    //   <li className="pagination__page">
-    //     <i className="fas fa-chevron-left" />
-    //   </li>
-    //   <li className="pagination__page">
-    //     1
-    //   </li>
-    //   <li className="pagination__page">
-    //     2
-    //   </li>
-    //   <li className="pagination__page">
-    //     3
-    //   </li>
-    //   <li className="pagination__page">
-    //     <i className="fas fa-chevron-right" />
-    //   </li>
-    // </ul>
     <div className="gallery__pagination">
-      {
-        currentPage > 1
-          ? <Link passHref={true} href={prevHref}>
-            <a style={{ color: 'blue', display: 'inline-block', margin: '25px 0 ' }}>{' < '}Página Anterior</a>
-          </Link>
-          : <span style={{ color: '#999' }}>{' < '}Página Anterior</span>
-      }
-      {
-        !isLastPage
-          ? <Link passHref={true} href={nextHref}>
-            <a style={{ color: 'blue', display: 'inline-block', margin: '25px 0 ' }}>Próxima Página {' > '}</a>
-          </Link>
-          : <span style={{ color: '#999' }}>Próxima Página {' > '}</span>
-      }
+      <PaginationItem text="< Página Anterior" href={currentPage < 2 ? '' : prevHref} />
+      <PaginationItem text="Próxima Página >" href={isLastPage ? '' : nextHref} />
     </div>
   );
 }
---
+++ components/Pagination/PaginationItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+
+export function PaginationItem({ text, href }) {
+  return (
+    href
+      ? <Link passHref={true} href={href}>
+        <a style={{ color: 'blue', display: 'inline-block', margin: '25px 0 ' }}>{text}</a>
+      </Link>
+      : <span style={{ color: '#999', display: 'inline-block', margin: '25px 0 ' }}>{text}</span>
+  );
+}
