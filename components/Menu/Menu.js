170cf25d1bb2523c9c8ee4bc58fe4901a3d10d89
Pedro Ramon <pedroramon.dev@gmail.com>
2021-12-28 09:51:44 -0300

    refactor: isolating menuItem from Menu

--- components/Menu/Menu.js
+++ components/Menu/Menu.js
@@ -1,12 +1,11 @@
 import React, { useContext, useRef } from 'react';
-import Link from 'next/link'
 import GlobalDataContext from '../../contexts/GlobalDataContext';
 
-import { linkTo } from '../../helpers'
 import MenuSeeAll from './MenuSeeAll';
-import MenuNivelOne from './MenuNivelOne';
 import { useAutoSizedMenu } from './useAutoSizedMenu';
 import { ButtonCloseMenuMobile } from './ButtonCloseMenuMobile';
+import UserTag from '../UserTag/UserTag'
+import { MenuItem } from './MenuItem';
 
 function Menu({ isFixed, isMenuMobileActive, closeMenuMobile }) {
   const {
@@ -23,55 +22,19 @@ function Menu({ isFixed, isMenuMobileActive, closeMenuMobile }) {
 
   return (
     <nav className={`header__menu ${isMenuMobileActive ? 'header__menu--active' : ''} ${isFixed ? 'fixed' : ''}`}>
-
       <ButtonCloseMenuMobile close={closeMenuMobile} />
-
       <ul className="container_serie-ds categorias_container--sd" ref={ulRef}>
-
         <MenuSeeAll itens={menu} />
-
         <li className="menu__item menu__login">
-          <a href="/login" className="item__link">
-            <img src="/images/login.svg" alt="Login" />
-            <span>Entre ou cadastre-se</span>
-          </a>
+          <UserTag isMobile={true} />
         </li>
-
         {menu && menu.map((linha) => (
-          <li className="menu__item menu__item--has-subcategoria" key={linha.id}>
-            <Link href={linkTo.category(linha)} passHref>
-              <a>
-                <img src={`${staticUrl}${linha.imagem}`} alt={linha.nome} />
-                <span>{linha.nome}</span>
-              </a>
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#filtros"
-              aria-controls="filtros"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <i className="fas fa-chevron-right" />
-            </button>
-            <ul className="item__subcategorias collapse navbar-collapse nvl-1" id="filtros">
-              <li className="category_father_name">
-                <Link href={linkTo.category(linha)} passHref>
-                  <a>{linha.nome}</a>
-                </Link>
-              </li>
-              {linha.subMenus && linha.subMenus.map((departamento) => (
-                <MenuNivelOne key={departamento.id} category={departamento} />
-              ))}
-            </ul>
-          </li>
+          <MenuItem linha={linha} staticUrl={staticUrl} />
         ))}
-
       </ul>
     </nav>
   );
 }
+
 
 export default Menu;
---
+++ components/Menu/MenuItem.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { linkTo } from '../../helpers';
+import MenuNivelOne from './MenuNivelOne';
+
+export function MenuItem({ linha, staticUrl }) {
+  const [isSubMenuActive, setIsSubMenuActive] = useState();
+
+  const subMenuClassName = isSubMenuActive ? 'menu__item--subcategoria-active' : '';
+
+  return (
+    <li className={`menu__item menu__item--has-subcategoria ${subMenuClassName}`} key={linha.id}>
+      <Link href={linkTo.category(linha)} passHref>
+        <a>
+          <img src={`${staticUrl}${linha.imagem}`} alt={linha.nome} />
+          <span>{linha.nome}</span>
+        </a>
+      </Link>
+
+      <button className="navbar-toggler" type="button" onClick={() => setIsSubMenuActive(!isSubMenuActive)}>
+        <i className="fas fa-chevron-right" />
+      </button>
+
+      <ul className="item__subcategorias navbar-collapse nvl-1">
+        <li className="category_father_name">
+          <Link href={linkTo.category(linha)} passHref>
+            <a>{linha.nome}</a>
+          </Link>
+        </li>
+        {linha.subMenus && linha.subMenus.map((departamento) => (
+          <MenuNivelOne key={departamento.id} category={departamento} />
+        ))}
+      </ul>
+    </li>
+  );
+}
