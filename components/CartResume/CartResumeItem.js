b6f6f444fad6fc80721c3f31ee90ff8066fe4621
Pedro Ramon <pedroramon.dev@gmail.com>
2021-12-28 10:07:58 -0300

    feat: allow cart itens to be edited in the sidebar

--- components/CartResume/CartResumeItem.js
+++ components/CartResume/CartResumeItem.js
@@ -1,20 +1,46 @@
-export default function CartResumeItem({ item }) {
-	return (
-		<li className="items__item">
-			<button className="item__delete">
-				<img src="/images/remove-cart.svg" alt="X" />
-			</button>
-			<img
-				className="item__image"
-				src={item.image}
-				alt="produto" />
-			<span className="item__name">
-				{item.name}
-			</span>
-			<div className="item__values">
-				<span className="values__qtd">{item.quantity} x</span>
-				<span className="values__price">{item.price}</span>
-			</div>
-		</li>
-	);
+export default function CartResumeItem({ item, onChange, decrement, increment, remove }) {
+  return (
+    <li className="items__item">
+      <img
+        className="item__image"
+        src={item.image}
+        alt="produto" />
+      <span className="item__name">
+        {item.name}
+      </span>
+      <div className="item__values">
+
+
+        <span className="item__control">
+          <button className="item__delete" onClick={() => remove(item)}>
+            <i className="far fa-times"></i>
+          </button>
+          <button
+            className="item__decrement"
+            onClick={() => decrement(item)}
+            disabled={item.quantity < 2}
+          >
+            <i className="far fa-chevron-down"></i>
+          </button>
+          <input
+            type="number"
+            step={1}
+            min={1}
+            value={item.quantity}
+            onChange={(evt) => onChange(item, evt.target.value)}
+          />
+          <button
+            className="item__increment"
+            onClick={() => increment(item)}
+          >
+            <i className="far fa-chevron-up"></i>
+          </button>
+        </span>
+        <span>
+          <span className="values__qtd">{item.quantity} x</span>
+          <span className="values__price">{item.price}</span>
+        </span>
+      </div>
+    </li>
+  );
 }
