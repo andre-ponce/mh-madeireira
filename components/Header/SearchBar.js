af9ca267760a506ed269eee02956f40c34bbca01
Pedro Ramon <pedroramon.dev@gmail.com>
2021-12-28 09:52:42 -0300

    refactor: removing deprecated search filters

--- components/Header/SearchBar.js
+++ components/Header/SearchBar.js
@@ -4,29 +4,6 @@ export default function SearchBar() {
   return (
     <div className="topbar__search">
       <div className="container_serie-ds d-flex align-items-center">
-        <div className="search__selections d-flex">
-          <select name="montadora" className="active-select" id="select-montadora">
-            <option value="">Selecione a Montadora</option>
-            <option value="">Volkswagen</option>
-          </select>
-
-          <select name="veiculo" className="d-none active-select" id="select-veiculo">
-            <option value="">Selecione o Veículo</option>
-            <option value="">Gol Quadrado</option>
-          </select>
-
-          <select name="ano" className="d-none active-select" id="select-ano">
-            <option value="">Ano</option>
-            <option value="">2000</option>
-          </select>
-
-          <select name="modelo" className="d-none active-select" id="select-modelo">
-            <option value="">Selecione o Modelo</option>
-            <option value="">4 portas</option>
-          </select>
-          <span className="active-line" />
-        </div>
-        <span className="txt-busca">Busca Avançada</span>
         <form className="search__input-search form-search d-flex">
           <input
             type="text"
