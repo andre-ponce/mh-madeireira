aba4d1593a4387f37a7892a1c0f25dbd93129acb
Pedro Ramon <pedroramon.dev@gmail.com>
2022-03-17 08:44:19 -0300

    feat: apply scripts on layout

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -4,12 +4,27 @@ import Header from '../Header';
 import Footer from '../Footer';
 import UserLoggedContext from '../../contexts/UserLoggedContext';
 import useAnimateOnRouteChange from '../../hooks/useAnimateOnRouteChange';
+import useInsecureRawScript from '../../hooks/useInsecureRawScript';
 import { useCart } from '../../hooks/useCart';
 
 export function Layout({ children, globalData }) {
   if (!globalData) {
     throw new Error('globalData is missing');
   }
+
+  const {
+    site: {
+      scriptChat,
+      scriptGoogleAnalytics,
+      scriptsHeaderGlobais,
+      scriptsBodyGlobais,
+    },
+  } = globalData;
+
+  useInsecureRawScript(scriptChat);
+  useInsecureRawScript(scriptGoogleAnalytics);
+  useInsecureRawScript(scriptsHeaderGlobais);
+  useInsecureRawScript(scriptsBodyGlobais);
 
   useAnimateOnRouteChange();
   const cart = useCart();
