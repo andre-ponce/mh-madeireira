13306e82b16ba7a261f9d8e22309e48ba5611598
Pedro Ramon <pedroramon.dev@gmail.com>
2022-04-21 14:29:15 -0300

    test: config jest to run without a .babelrc

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,7 @@ module.exports = {
     '<rootDir>/.next/',
     '<rootDir>/node_modules/',
   ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
 };
