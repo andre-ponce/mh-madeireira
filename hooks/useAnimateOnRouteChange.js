4697e097506c067eb64f59e8d1743e0ee41f271e
Pedro Ramon <pedroramon.dev@gmail.com>
2022-06-10 16:07:50 -0300

    fix: active animate on route change and middleware

--- hooks/useAnimateOnRouteChange.js
+++ hooks/useAnimateOnRouteChange.js
@@ -6,47 +6,50 @@ export default function useAnimateOnRouteChange() {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
-  // useEffect(() => {
-  //   const handleStart = () => {
-  //     setIsRouting(true);
-  //     const aosElements = document.querySelectorAll('[data-aos]');
-  //     aosElements.forEach(el => {
-  //       el.classList.remove('aos-init');
-  //       el.classList.remove('aos-animate');
-  //     })
-  //   }
+  useEffect(() => {
+    const loadingClass = 'routing';
+    const { body } = document;
 
-  //   const handleStop = () => {
-  //     setIsRouting(false);
-  //     AOS.refresh(true);
-  //   }
+    const setIsRouting = (loading) => {
+      setIsLoading(loading);
+      if (loading) {
+        body.classList.add(loadingClass);
+      } else {
+        body.classList.remove(loadingClass);
+      }
+    };
 
-  //   const handleError = () => {
-  //     setIsRouting(false);
-  //     AOS.refresh(true);
-  //   }
+    const handleStart = () => {
+      setIsRouting(true);
+      // const aosElements = document.querySelectorAll('[data-aos]');
+      // aosElements.forEach(el => {
+      //   el.classList.remove('aos-init');
+      //   el.classList.remove('aos-animate');
+      // })
+    };
 
-  //   const loadingClass = 'routing';
-  //   const body = document.body;
-  //   const setIsRouting = (isLoading) => {
-  //     setIsLoading(isLoading);
-  //     isLoading
-  //       ? body.classList.add(loadingClass)
-  //       : body.classList.remove(loadingClass);
-  //   }
+    const handleStop = () => {
+      setIsRouting(false);
+      // AOS.refresh(true);
+    };
 
-  //   router.events.on('routeChangeStart', handleStart);
-  //   router.events.on('routeChangeComplete', handleStop);
-  //   router.events.on('routeChangeError', handleError);
+    const handleError = () => {
+      setIsRouting(false);
+      // AOS.refresh(true);
+    };
 
-  //   AOS.init();
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleError);
 
-  //   return () => {
-  //     router.events.off('routeChangeStart', handleStart);
-  //     router.events.off('routeChangeComplete', handleStop);
-  //     router.events.off('routeChangeError', handleError);
-  //   }
-  // }, [router]);
+    AOS.init();
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleError);
+    };
+  }, [router]);
 
   return isLoading;
 }
