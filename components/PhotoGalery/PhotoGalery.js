908f6abed751b3766b535811f091649c15f60e13
Pedro Ramon <pedroramon.dev@gmail.com>
2022-09-23 13:41:24 -0300

    fix: product galery

--- components/PhotoGalery/PhotoGalery.js
+++ components/PhotoGalery/PhotoGalery.js
@@ -1,6 +1,5 @@
 import { url } from '@/services/statics.service';
 import Image from 'next/image';
-import { useEffect } from 'react';
 import Slick from 'react-slick';
 import { Loader } from './Loader';
 
@@ -11,52 +10,38 @@ const defaultSettingsGalery = {
   autoplay: false,
 };
 
+const SliderWrap = ({ children, ...settings }) => (
+  <Slick className="slider__product__galery" {...settings}>{children}</Slick>
+);
+
 export function PhotoGalery({ photos, slickSettings }) {
   const settings = {
     ...defaultSettingsGalery,
     ...slickSettings,
   };
 
-  useEffect(() => {
-    if (!photos) {
-      return;
-    }
-
-    const slides = document.querySelectorAll('.slider__product__galery .slick-slide:not(.slick-cloned) img');
-    const li = document.querySelectorAll('.slider__product__galery .slick-dots > li');
-
-    slides.forEach((img, index) => {
-      if (li[index]) {
-        li[index].style.backgroundImage = `url("${img.src}")`;
-      }
-    });
-  }, [photos]);
-
-  const galery = [...(photos || [])];
+  if (photos === undefined) return (<SliderWrap {...settings}><div><Loader /></div></SliderWrap>);
+  if (!photos.length) return (<SliderWrap {...settings}><Photo /></SliderWrap>);
 
   return (
-    photos === undefined
-      ? (
-        <Slick className="slider__product__galery" {...settings}>
-          <div>
-            <Loader />
-          </div>
-        </Slick>
-      )
-      : (
-        <Slick className={`slider__product__galery ${photos.length > 1 ? 'slider__product__galery--has-thumbs' : ''}`} {...settings}>
-          {galery.map((photo) => (
-            <div className="slider__product__galery--photo" key={photo.id}>
-              <Image
-                src={url.imageProduct(photo.url)}
-                alt={photo.textoAlternativo}
-                title={photo.titulo}
-                height={400}
-                width={400}
-              />
-            </div>
-          ))}
-        </Slick>
-      )
+    <SliderWrap {...settings}>
+      {photos.map((photo) => <Photo key={photo.id} {...photo} />)}
+    </SliderWrap>
+  );
+}
+
+function Photo({ url: photoUrl, textoAlternativo, titulo }) {
+  return (
+    <div>
+      <div className="slider__product__galery--photo">
+        <Image
+          src={url.imageProduct(photoUrl)}
+          alt={textoAlternativo}
+          title={titulo}
+          height={400}
+          width={400}
+        />
+      </div>
+    </div>
   );
 }
