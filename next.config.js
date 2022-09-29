9f2d576cdca66f01c40405bdfe8a9f73cfb62cb6
Pedro Ramon <pedroramon.dev@gmail.com>
2022-09-29 16:30:59 -0300

    feat: add redirect to old routes

--- next.config.js
+++ next.config.js
@@ -5,4 +5,43 @@ module.exports = {
       'maximaweb.s3.amazonaws.com',
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/linha/:id/:slug',
+        destination: '/:slug/l/:id',
+        permanent: true,
+      },
+      {
+        source: '/sub-departamento/:id/:slug',
+        destination: '/:slug/s/:id',
+        permanent: true,
+      },
+      {
+        source: '/departamento/:id/:slug',
+        destination: '/:slug/d/:id',
+        permanent: true,
+      },
+      {
+        source: '/marca/:id/:slug',
+        destination: '/:slug/m/:id',
+        permanent: true,
+      },
+      {
+        source: '/produto/:id/:sub/:dep/:slug',
+        destination: '/:slug/p/:id',
+        permanent: true,
+      },
+      {
+        source: '/produto/:id/:dep/:slug',
+        destination: '/:slug/p/:id',
+        permanent: true,
+      },
+      {
+        source: '/produto/:id/:slug',
+        destination: '/:slug/p/:id',
+        permanent: true,
+      },
+    ];
+  },
 };
