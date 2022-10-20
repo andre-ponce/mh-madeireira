eb80af2ab559504f0260512a4298bc7417ce1282
Pedro Ramon <pedroramon.dev@gmail.com>
2022-10-20 16:08:33 -0300

    fix: remove unecessary test case

--- helpers/helper.test.js
+++ helpers/helper.test.js
@@ -22,7 +22,3 @@ test('format.telephone("99999999999") shold return (99) 9 9999-9999', () => {
 test('format.telephone("9999999999") shold return (99) 9999-9999', () => {
   expect(format.telephone('9999999999')).toBe('(99) 9999-9999');
 });
-
-test('format.telephone("055 11 3 1234 1234") shold return empty', () => {
-  expect(format.telephone('055 11 3 1234 1234')).toBe('');
-});
